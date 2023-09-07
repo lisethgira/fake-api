b9af600a83872a43fb13cce1d839ee3ca1e5a9e6
Liseth Giraldo <liseth.giraldo@rocketfy.co>
2023-09-06 23:12:42 -0500

    componente products
    se deja solo componentes de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent,],
   imports: [BrowserModule,
     AppRoutingModule,
     HttpClientModule],
